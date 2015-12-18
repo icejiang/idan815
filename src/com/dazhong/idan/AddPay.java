a64751f273a8bb6c1cb50946eb07802722b70783
icejiang <jxb5928@hotmail.com>
2015-12-18 16:55:38 +0800

    123

--- src/com/dazhong/idan/AddPay.java
+++ src/com/dazhong/idan/AddPay.java
@@ -1,5 +1,6 @@
 package com.dazhong.idan;
 
+import android.R.integer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddPay extends Activity {
 
@@ -17,6 +19,9 @@ public class AddPay extends Activity {
 	private EditText et_parking;
 	private EditText et_other;
 	private EditText et_hotel;
+	private EditText et_alter;
+	private EditText et_routeOn;
+	private EditText et_routeOff;
 	private TextView bt_save;
 	private ImageView iv_return;
 	public final static String key_road = "ROAD";
@@ -24,7 +29,13 @@ public class AddPay extends Activity {
 	public final static String key_parking = "PARKING";
 	public final static String key_other = "OTHER";
 	public final static String key_hotel = "HOTEL";
+	public final static String key_alter = "ALTER";
+	public final static String key_routeOn = "ROUTEON";
+	public final static String key_routeOff = "ROUTEOFF";
 	public final static int RESULT_CODE=1;
+	private getStateInfo myGetStateInfo;
+	private StateInfo myStateInfo;
+	private NoteInfo noteInfo;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +44,68 @@ public class AddPay extends Activity {
 		setContentView(R.layout.add_pay);
 		ActivityControler.addActivity(this);
 		findView();
+		try {
+			myGetStateInfo = getStateInfo.getInstance(getApplicationContext());
+			myStateInfo = myGetStateInfo.getStateinfo();
+			myStateInfo.setCurrentState(17);
+			noteInfo = myStateInfo.getCurrentNote();
+			myGetStateInfo.setStateinfo(myStateInfo);
+			
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		et_routeOn.setText(noteInfo.getRouteBegin());
+		et_routeOff.setText(noteInfo.getRouteEnd());
 		
 		bt_save.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 
+				String alter = et_alter.getText().toString();
+				if (!alter.equals("")&&!(alter == null)){
+					double price_all = noteInfo.getFeePrice();
+					if(Double.valueOf(alter)>price_all){
+						Toast.makeText(getApplicationContext(), "修正金额路单总价,请确认输入", Toast.LENGTH_SHORT).show();
+						return;
+					}
+				}
+				String routeOn = et_routeOn.getText().toString();
+				String routeOff = et_routeOff.getText().toString();
+				String previousOn = noteInfo.getRouteBegin();
+				if(!routeOn.equals("")&&!(routeOn == null)){
+					String beginKMSofDay = myStateInfo.getBeginKMsOfToday();
+					int kmsDay = 0;
+					if(beginKMSofDay.equals("")||beginKMSofDay == null){
+						
+					} else {
+						kmsDay = Integer.parseInt(beginKMSofDay);
+					}
+					if(Integer.parseInt(routeOn)<kmsDay){
+						Toast.makeText(getApplicationContext(), "输入路码小于今日上车路码,请确认输入", Toast.LENGTH_SHORT).show();
+						return;
+					}
+				} else {
+					previousOn = routeOn;
+				}
+				if(!routeOff.equals("")&&!(routeOff == null)){
+					if(Integer.parseInt(routeOff)<Integer.parseInt(previousOn)){
+						Toast.makeText(getApplicationContext(), "输入路码小于上车路码,请确认输入", Toast.LENGTH_SHORT).show();
+						return;
+					} 
+				}
+				
+				
 				Bundle bundle = new Bundle();
 				bundle.putString(key_road, et_road.getText().toString());
 				bundle.putString(key_meals, et_eat.getText().toString());
 				bundle.putString(key_parking, et_parking.getText().toString());
 				bundle.putString(key_other, et_other.getText().toString());
 				bundle.putString(key_hotel, et_hotel.getText().toString());
+				bundle.putString(key_alter, alter);
+				bundle.putString(key_routeOn, routeOn);
+				bundle.putString(key_routeOff, routeOff);
 				Intent intent = new Intent();
 				intent.putExtras(bundle);
 	            setResult(RESULT_CODE, intent);  
@@ -71,5 +132,8 @@ public class AddPay extends Activity {
 		bt_save = (TextView) findViewById(R.id.bt_save);
 		et_hotel = (EditText) findViewById(R.id.pay_hotel);
 		iv_return = (ImageView) findViewById(R.id.return_addPay);
+		et_alter = (EditText) findViewById(R.id.price_alter);
+		et_routeOn = (EditText) findViewById(R.id.route_on);
+		et_routeOff = (EditText) findViewById(R.id.route_off);
 	}
 }
